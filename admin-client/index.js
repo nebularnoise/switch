286e4daa1ebb1a1cb61b4f7e294c3b23ad24a671
Thibault Geoffroy <admin@nebularnoise.com>
2019-01-06 17:38:15 +0100

    :construction: prettier bs

--- admin-client/index.js
+++ admin-client/index.js
@@ -1,50 +1,49 @@
-const get = require('axios').get
-const jwt = require('jsonwebtoken')
-const readFileSync = require('fs').readFileSync
+const get = require("axios").get;
+const jwt = require("jsonwebtoken");
+const readFileSync = require("fs").readFileSync;
 
-const { SERVER_URL, API_PORT, WEBSOCKET_PORT } = require('../config.js')
+const { SERVER_URL, API_PORT, WEBSOCKET_PORT } = require("../config.js");
 
-const clientName = 'switch'
-const cert = readFileSync('./jwtRS256.key')
+const clientName = "switch";
+const cert = readFileSync("./jwtRS256.key");
 
 const token = jwt.sign(
-	{
-		name: 'switch',
-		admin: true
-	},
-	cert,
-	{ algorithm: 'RS256' }
-)
+  {
+    name: "switch",
+    admin: true
+  },
+  cert,
+  { algorithm: "RS256" }
+);
 
-var buttons = require('rpi-gpio-buttons')([11], {debounce: 500});
+var buttons = require("rpi-gpio-buttons")([11], { debounce: 500 });
 
 // bind to the clicked event and check for the assigned pins when clicked
-buttons.on('clicked', function (pin) {
-  get(`http://${SERVER_URL}:${API_PORT}/api/is-open`)
-	.then(function (isOpen) {
+buttons.on("clicked", function(pin) {
+  get(`http://${SERVER_URL}:${API_PORT}/api/is-open`).then(function(isOpen) {
     isOpen ? close() : open();
-  })
+  });
 });
 
 function open() {
-	console.log('OPENING')
-	get(`http://${SERVER_URL}:${API_PORT}/api/open`, {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			Authorization: `Bearer ${token}`
-		}
-	})
+  console.log("OPENING");
+  get(`http://${SERVER_URL}:${API_PORT}/api/open`, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Authorization: `Bearer ${token}`
+    }
+  });
 }
 
 function close() {
-	console.log('CLOSING')
-	get(`http://${SERVER_URL}:${API_PORT}/api/close`, {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			Authorization: `Bearer ${token}`
-		}
-	})
+  console.log("CLOSING");
+  get(`http://${SERVER_URL}:${API_PORT}/api/close`, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      Authorization: `Bearer ${token}`
+    }
+  });
 }
 
-module.exports.open = open
-module.exports.close = close
+module.exports.open = open;
+module.exports.close = close;
